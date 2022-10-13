b2dbf3ec674870d91091dd612ef624f8b21827fe
Dmytro Stupak <stupakbf@gmail.com>
2022-10-13 16:02:45 +0300

    Update slice.js

--- src/redux/Auth/slice.js
+++ src/redux/Auth/slice.js
@@ -6,27 +6,51 @@ const initialState = {
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
+  loading: false,
+  error: null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: {
+    [register.pending]: state => ({ ...state, loading: true, error: null }),
     [register.fulfilled](state, action) {
+      console.log(state);
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
+    [register.rejected]: (state, { payload }) => ({
+      ...state,
+      loading: false,
+      error: payload,
+    }),
+
+    [logIn.pending]: state => ({ ...state, loading: true, error: null }),
     [logIn.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
+    [logIn.rejected]: (state, { payload }) => ({
+      ...state,
+      loading: false,
+      error: payload,
+    }),
+
+    [logOut.pending]: state => ({ ...state, loading: true, error: null }),
     [logOut.fulfilled](state) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLoggedIn = false;
     },
+    [logOut.rejected]: (state, { payload }) => ({
+      ...state,
+      loading: false,
+      error: payload,
+    }),
+
     [refreshUser.pending](state) {
       state.isRefreshing = true;
     },
